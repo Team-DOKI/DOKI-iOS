7b11fb43a3f870ec4187417a7e614f3341f45471
sem-git <lsmsm0327@naver.com>
2025-06-21 01:28:17 +0900

    [Init] 프로젝트 생성 및 초기 세팅

---
+++ DoggyWalker/DoggyWalker/Application/DoggyWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggyWalkerApp.swift
+//  DoggyWalker
+//
+//  Created by 이세민 on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DoggyWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
