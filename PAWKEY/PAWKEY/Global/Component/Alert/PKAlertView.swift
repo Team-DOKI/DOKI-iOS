f350eeebf1c0c36594a2fb450b0425bf46ada424
kwonseokki <kwons3327@gmail.com>
2025-07-05 17:59:26 +0900

    [Add] PKAlertView 추가

---
+++ PAWKEY/PAWKEY/Global/Component/Alert/PKAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  PKAlertView.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/5/25.
+//
+
+import SwiftUI
+
+struct PKAlertView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PKAlertView()
+}
