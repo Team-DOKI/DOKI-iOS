e349c80a93421ad414d9aa8aae0f9dbbae65d35f
kwonseokki <kwons3327@gmail.com>
2025-07-07 02:10:12 +0900

    [Feat] #7 위치 권한 요청 함수 추가

--- PAWKEY/PAWKEY/Global/Manager/LocationManager.swift
+++ PAWKEY/PAWKEY/Global/Manager/LocationManager.swift
@@ -5,3 +5,24 @@
 //  Created by 권석기 on 7/7/25.
 //
 
+import CoreLocation
+
+final class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    
+    private let locationManager = CLLocationManager()
+    
+    private override init() {
+        super.init()
+        setLocationManager()
+    }
+    
+    func requestLocationPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func setLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 가장 높은수준의 정확도
+    }
+}
