26c4caec5d3a2ebd2fe7f1195028f9596e2ba753
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-06 17:38:16 +0900

    [Add] #4 SubmitButton 파일 추가

---
+++ PAWKEY/PAWKEY/Global/Component/Button/SubmitButton.swift
@@ -0,0 +1,18 @@
+//
+//  SubmitButton.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/6/25.
+//
+
+import SwiftUI
+
+struct SubmitButton: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SubmitButton()
+}
