350e21d417f49808c1b261a7eaa0382607d4a3ee
sem-git <lsmsm0327@naver.com>
2025-07-07 14:01:08 +0900

    [Feat] #5 Tab별 Coordinator 구조 추가 (Flow)

---
+++ PAWKEY/PAWKEY/Global/Router/WalkFlow.swift
@@ -0,0 +1,45 @@
+//
+//  WalkFlow.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/6/25.
+//
+
+import SwiftUI
+
+struct WalkFlow: View {
+    @EnvironmentObject var router: TabRouter<WalkScreen>
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            CourseListView()
+                .navigationDestination(for: WalkScreen.self) { screen in
+                    build(screen: screen)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private func build(screen: WalkScreen) -> some View {
+        switch screen {
+        case .courseList:
+            CourseListView()
+        case .courseDetail(let id):
+            CourseDetailView(courseId: id)
+            
+        case .walkCourse(let id):
+            WalkCourseView(courseId: id)
+        case .walkCompletion(let id):
+            WalkCompletionView(courseId: id)
+        case .archive(let id):
+            ArchiveView(archiveId: id)
+            
+        case .sharedWalkCourse(let id):
+            SharedWalkCourseView(sharedCourseId: id)
+        case .sharedWalkCompletion(let id):
+            SharedWalkCompletionView(sharedWalkId: id)
+        case .reviewWrite(let id):
+            ReviewWriteView(reviewId: id)
+        }
+    }
+}
