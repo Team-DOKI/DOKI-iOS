4883c253ff5f1a43234f778bb675777cf534a616
sem-git <lsmsm0327@naver.com>
2025-07-07 13:58:40 +0900

    [Feat] #5 화면 전환 식별자 Screen enum 정의

---
+++ PAWKEY/PAWKEY/Global/Router/ScreenEnum.swift
@@ -0,0 +1,32 @@
+//
+//  ScreenEnum.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/7/25.
+//
+
+import Foundation
+
+enum HomeScreen: Hashable {
+    case home
+    case changeMyArea
+}
+
+enum WalkScreen: Hashable {
+    case courseList
+    case courseDetail(id: Int)
+
+    case walkCourse(id: Int)
+    case walkCompletion(id: Int)
+    case archive(id: Int)
+    
+    case sharedWalkCourse(id: Int)
+    case sharedWalkCompletion(id: Int)
+    case reviewWrite(id: Int)
+}
+
+enum MyPageScreen: Hashable {
+    case myPage
+    case userProfile
+    case petProfile
+}
