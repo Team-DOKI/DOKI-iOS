59dfa2187d8a122df09de0aff1b36faed3905018
kwonseokki <kwons3327@gmail.com>
2025-07-07 21:43:57 +0900

    [Feat] #18 Chip 컴포넌트 추가

---
+++ PAWKEY/PAWKEY/Global/Component/Chip.swift
@@ -0,0 +1,29 @@
+//
+//  Chip.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/7/25.
+//
+
+import SwiftUI
+
+struct Chip: View {
+    let title: String
+    var isActive: Bool = false
+    var body: some View {
+        Text(title)
+            .font(.pretendard(size: 14, weight: .medium))
+            .foregroundStyle(isActive ? .black : .gray300)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 4)
+            .background(.white)
+            .cornerRadius(4)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.gray
+        Chip(title: "선택된 옵션 입력")
+    }
+}
