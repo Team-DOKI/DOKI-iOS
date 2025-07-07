e629918d710d9e4a2611bc8e395a062375d44e4c
sem-git <lsmsm0327@naver.com>
2025-07-08 06:25:48 +0900

    [Feat] #14 거리/시간/걸음수 통계 UI 추가

--- PAWKEY/PAWKEY/Presentation/Walk/Walk/View/WalkCourseView.swift
+++ PAWKEY/PAWKEY/Presentation/Walk/Walk/View/WalkCourseView.swift
@@ -22,7 +22,51 @@ struct WalkCourseView: View {
             .edgesIgnoringSafeArea(.all)
             
             VStack {
+                HStack(spacing: 32) {
+                    VStack {
+                        Text("거리 (km)")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                        Text(String(format: "%.1f", viewModel.distanceInKilometers))
+                            .font(.title3).bold()
+                            .foregroundColor(.green)
+                    }
+                    .frame(width: 66)
+                    
+                    VStack {
+                        Text("시간 (분)")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                        Text(viewModel.elapsedTimeString)
+                            .font(.title3).bold()
+                            .foregroundColor(.green)
+                    }
+                    .frame(width: 66)
+                    
+                    VStack {
+                        Text("걸음 수 (걸음)")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                        Text("\(viewModel.stepCount)")
+                            .font(.title3).bold()
+                            .foregroundColor(.green)
+                    }
+                    .frame(width: 66)
+                }
+                .padding(.vertical, 14)
+                .padding(.horizontal, 32)
+                .frame(maxWidth: .infinity, minHeight: 84)
+                .background(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .inset(by: 0.5)
+                        .stroke(Color(red: 0.09, green: 0.74, blue: 0.18), lineWidth: 1)
+                )
+                .padding(.top, 16)
+                .padding(.horizontal, 23)
+                
                 Spacer()
+                
                 SubmitButton(
                     title: "종료하기",
                     isDisabled: false,
