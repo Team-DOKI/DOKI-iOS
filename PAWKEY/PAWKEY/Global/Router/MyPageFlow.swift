88da80c0b072d168b684b8003cfa60710399343e
sem-git <lsmsm0327@naver.com>
2025-07-07 14:01:08 +0900

    [Feat] #5 Tab별 Coordinator 구조 추가 (Flow)

---
+++ PAWKEY/PAWKEY/Global/Router/MyPageFlow.swift
@@ -0,0 +1,33 @@
+//
+//  MyPageFlow.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/6/25.
+//
+
+import SwiftUI
+
+struct MyPageFlow: View {
+    @EnvironmentObject var router: TabRouter<MyPageScreen>
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            MyPageView()
+                .navigationDestination(for: MyPageScreen.self) { screen in
+                    build(screen: screen)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private func build(screen: MyPageScreen) -> some View {
+        switch screen {
+        case .myPage:
+            MyPageView()
+        case .userProfile:
+            UserProfileView()
+        case .petProfile:
+            PetProfileView()
+        }
+    }
+}
