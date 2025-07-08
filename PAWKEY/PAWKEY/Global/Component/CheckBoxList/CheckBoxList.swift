f3d15b6d910bd33e8e7d17aafae91a3648d82989
kwonseokki <kwons3327@gmail.com>
2025-07-08 16:33:15 +0900

    [Feat] #21 선택된 항목 개수 표시

--- PAWKEY/PAWKEY/Global/Component/CheckBoxList/CheckBoxList.swift
+++ PAWKEY/PAWKEY/Global/Component/CheckBoxList/CheckBoxList.swift
@@ -11,16 +11,22 @@ struct CheckBoxList: View {
     @Binding var isExpanded: Bool
     
     let title: String
-    var items: [CheckOption]
     
+    var items: [CheckOption]
     var action: ((Int) -> Void)?
     
     var body: some View {
         VStack {
             HStack {
+                let itemCount = items.filter{ $0.isSelected}.count
                 Text(title)
                     .font(.body_16_sb)
                 Spacer()
+                if itemCount > 0 {
+                    Text("\(itemCount)개 항목 선택")
+                        .font(.caption_12_sb)
+                        .foregroundStyle(.green)
+                }
                 Image(.arrowDownBlack)
                     .rotationEffect(isExpanded ? .degrees(-180) : .degrees(0))
             }
