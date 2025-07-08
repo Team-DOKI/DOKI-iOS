92f6619696fe4d5ea99fe5ed59b124dd1f01232e
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-08 21:32:30 +0900

    [Add] #22 후기 차트뷰 컴포넌트 추가

---
+++ PAWKEY/PAWKEY/Global/Component/ReviewRatingBar.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewRatingBar.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/8/25.
+//
+
+import SwiftUI
+
+struct ReviewRatingBar: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ReviewRatingBar()
+}
