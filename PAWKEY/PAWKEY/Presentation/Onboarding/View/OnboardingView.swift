dfbbf6e331a918c9855cd55664f3b85bb2e66af9
kwonseokki <kwons3327@gmail.com>
2025-07-09 03:44:51 +0900

    [Feat] #43 계정생성 화면 구현

--- PAWKEY/PAWKEY/Presentation/Onboarding/View/OnboardingView.swift
+++ PAWKEY/PAWKEY/Presentation/Onboarding/View/OnboardingView.swift
@@ -9,6 +9,28 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text("온보딩")
+        VStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("안녕하세요.")
+                Text("우리 강아지를 위한 산책,")
+                HStack {
+                    Text("PAWKEY")
+                        .foregroundStyle(.green500)
+                    Text("와 함께해요!")
+                }
+            }
+            .font(.head_22_sb)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 50)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                CTAButton(title: "신규계정으로 회원가입")
+                CTAButton(title: "기존 계정으로 로그인", buttonStyle: .text)
+            }
+            .padding(.bottom, 47)
+        }
+        .padding(.horizontal, 16)
     }
 }
