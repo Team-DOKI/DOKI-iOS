65b7eefbb05df19f5651b21a48e2fe919d57e147
kwonseokki <kwons3327@gmail.com>
2025-07-08 22:51:03 +0900

    [Feat] #22 순위에 따라서 ProgessBar Color 변경

--- PAWKEY/PAWKEY/Global/Component/ReviewRatingBar.swift
+++ PAWKEY/PAWKEY/Global/Component/ReviewRatingBar.swift
@@ -10,27 +10,43 @@ import SwiftUI
 struct ReviewRatingBar: View {
     let title: String
     let rating: Double
+    let rank: Int
+    
+    var progessBarColor: Color {
+        switch rank {
+        case 1:
+            return .green500
+        case 2:
+            return .green400
+        default:
+            return .green300
+        }
+    }
+    
     var body: some View {
-        ZStack(alignment: .leading) {
-            GeometryReader { proxy in
+        VStack {
+            ZStack(alignment: .leading) {
                 RoundedRectangle(cornerRadius: 6)
                     .frame(height: 37)
                     .foregroundStyle(.pawkeyWhite2)
                 
-                RoundedRectangle(cornerRadius: 6)
-                    .frame(width: proxy.size.width * rating, height: 37)
-                    .foregroundStyle(.green)
+                GeometryReader { proxy in
+                    RoundedRectangle(cornerRadius: 6)
+                        .frame(width: proxy.size.width * rating, height: 37)
+                        .foregroundStyle(progessBarColor)
+                        .frame(height: 37)
+                }
                 Text(title)
                     .font(.caption_12_sb)
+                    .foregroundStyle(.pawkeyWhite1)
                     .frame(height: 37, alignment: .center)
                     .padding(.leading, 13)
             }
-            .fixedSize(horizontal: false, vertical: true)
         }
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
 #Preview {
-    ReviewRatingBar(title: "후기 옵션 입력", rating: 0.8)
-        .padding(.horizontal, 20)
+    ReviewRatingBar(title: "후기 옵션 입력", rating: 0.8, rank: 1)
 }
