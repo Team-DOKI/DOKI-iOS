7a1e28a0d30fdd4613dfd6970cd160e6777bdec2
sem-git <lsmsm0327@naver.com>
2025-07-09 03:35:17 +0900

    [Refactor] #36 WalkingMapView 디자인 시스템 적용

--- PAWKEY/PAWKEY/Presentation/Walk/Walk/View/WalkingMapView.swift
+++ PAWKEY/PAWKEY/Presentation/Walk/Walk/View/WalkingMapView.swift
@@ -48,8 +48,8 @@ struct WalkingMapView: UIViewRepresentable {
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             if let polyline = overlay as? MKPolyline {
                 let renderer = MKPolylineRenderer(polyline: polyline)
-                renderer.strokeColor = .green
-                renderer.lineWidth = 7
+                renderer.strokeColor = .green500
+                renderer.lineWidth = 6
                 return renderer
             }
             return MKOverlayRenderer()
