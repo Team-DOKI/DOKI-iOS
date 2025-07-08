7a21bf6b92c765e686c956a286fdc650605db66e
sem-git <lsmsm0327@naver.com>
2025-07-08 20:46:00 +0900

    [Feat] #14 TabBar 상태 관리용 ObservableObject 추가

---
+++ PAWKEY/PAWKEY/Global/Router/TabBarState.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarState.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/8/25.
+//
+
+import SwiftUI
+
+final class TabBarState: ObservableObject {
+    @Published var isHidden: Bool = false
+}
