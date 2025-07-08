8110ff2eec2719448e094340a1866d9a6ff17cf9
kwonseokki <kwons3327@gmail.com>
2025-07-08 11:04:54 +0900

    [Feat] #19 비밀번호 입력창 표시여부 토글 기능 추가

--- PAWKEY/PAWKEY/Global/Component/TextField/PKTextField.swift
+++ PAWKEY/PAWKEY/Global/Component/TextField/PKTextField.swift
@@ -14,7 +14,7 @@ struct PKTextField: View {
         case password
     }
     
-    
+    @State var isShowPassword = false
     @Binding var text: String
     @FocusState private var isFocused: Bool
     
@@ -25,15 +25,33 @@ struct PKTextField: View {
         ZStack {
             Group {
                 if type == .password {
-                    SecureField(text: $text) {}
-                        .focused($isFocused)
+                    HStack {
+                        ZStack {
+                            SecureField("", text: $text)
+                                .opacity(isShowPassword ? 0 : 1)
+                                .focused($isFocused)
+                                .font(.pretendard(size: 14, weight: .regular))
+                            
+                            
+                            TextField(placeholder ?? "입력해주세요", text: $text)
+                                .opacity(isShowPassword ? 1 : 0)
+                                .focused($isFocused)
+                                .font(.pretendard(size: 14, weight: .regular))
+                        }
+                        
+                        Button {
+                            isShowPassword.toggle()
+                        } label: {
+                            Text(isShowPassword ? "hide" : "show")
+                        }
+                    }
                 } else {
                     TextField(text: $text) {
                         Text(placeholder ?? "입력해주세요")
                             .font(.pretendard(size: 14, weight: .regular))
                             .foregroundStyle(.gray200)
                     }
-                    .focused($isFocused)                    
+                    .focused($isFocused)
                 }
             }
             .padding(.leading, 20)
@@ -48,6 +66,6 @@ struct PKTextField: View {
 }
 
 #Preview {
-    PKTextField(text: .constant(""), placeholder: "비번입력", type: .normal)
+    PKTextField(text: .constant(""), placeholder: "비번입력", type: .password)
         .padding(.horizontal, 20)
 }
