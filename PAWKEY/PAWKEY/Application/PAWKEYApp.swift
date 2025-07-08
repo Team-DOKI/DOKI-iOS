82b015453c8bb30b584f87ecc8cd15c721a5788f
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-08 15:43:37 +0900

    [Feat] #21 AccordionView 추가

--- PAWKEY/PAWKEY/Application/PAWKEYApp.swift
+++ PAWKEY/PAWKEY/Application/PAWKEYApp.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 @main
 struct PAWKEYApp: App {
+    @State var isExpanded: Bool = false
+    @State var isExpanded2: Bool = false
     var body: some Scene {
         WindowGroup {
-            TabView()
-                .onAppear {
-                    LocationManager.shared.requestLocationPermission()
-                }
+            VStack {
+                AccordionView(isExpanded: $isExpanded, title: "산책 소요 시간", items: ["20분 이내", "21-40분", "41-60분", "1시간 이상"])
+                Divider()
+                AccordionView(isExpanded: $isExpanded2, title: "산책 소요 시간", items: ["20분 이내", "21-40분", "41-60분", "1시간 이상"])
+            }
+//            TabView()
+//                .onAppear {
+//                    LocationManager.shared.requestLocationPermission()
+//                }
         }
     }
 }
