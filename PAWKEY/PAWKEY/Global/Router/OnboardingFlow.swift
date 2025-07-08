b30a5440481fee69f2d7c51c4feffb6259f83f06
kwonseokki <kwons3327@gmail.com>
2025-07-09 04:02:56 +0900

    [Feat] #43 로그인 화면 연결

---
+++ PAWKEY/PAWKEY/Global/Router/OnboardingFlow.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingFlow.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/9/25.
+//
+
+import SwiftUI
+
+struct OnboardingFlow: View {
+    @EnvironmentObject var router: TabRouter<OnboardingScreen>
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            OnboardingView()
+                .navigationDestination(for: OnboardingScreen.self) { screen in
+                    build(screen: screen)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private func build(screen: OnboardingScreen) -> some View {
+        switch screen {
+        case .login:
+            LoginView(isLoggedIn: .constant(false))
+        }
+    }
+}
