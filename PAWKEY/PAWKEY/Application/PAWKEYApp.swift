5714848d44dfb212e2e0b74ca4a5857dcd9cdc7f
kwonseokki <kwons3327@gmail.com>
2025-07-08 18:36:16 +0900

    [Add] #21 필터링 관련 모델 추가

--- PAWKEY/PAWKEY/Application/PAWKEYApp.swift
+++ PAWKEY/PAWKEY/Application/PAWKEYApp.swift
@@ -9,19 +9,12 @@ import SwiftUI
 
 @main
 struct PAWKEYApp: App {
-    @State var isExpanded: Bool = false
-    @State var isExpanded2: Bool = false
     var body: some Scene {
         WindowGroup {
-            VStack {
-                AccordionView(isExpanded: $isExpanded, title: "산책 소요 시간", items: ["20분 이내", "21-40분", "41-60분", "1시간 이상"])
-                Divider()
-                AccordionView(isExpanded: $isExpanded2, title: "산책 소요 시간", items: ["20분 이내", "21-40분", "41-60분", "1시간 이상"])
-            }
-//            TabView()
-//                .onAppear {
-//                    LocationManager.shared.requestLocationPermission()
-//                }
+            TabView()
+                .onAppear {
+                    LocationManager.shared.requestLocationPermission()
+                }
         }
     }
 }
