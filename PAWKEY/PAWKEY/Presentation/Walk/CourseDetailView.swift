805cbc05b6640786402f6e37dcf3c290d0adb165
sem-git <lsmsm0327@naver.com>
2025-07-10 02:52:26 +0900

    [Refactor] #36 LocationDateView 분리

--- PAWKEY/PAWKEY/Presentation/Walk/CourseDetailView.swift
+++ PAWKEY/PAWKEY/Presentation/Walk/CourseDetailView.swift
@@ -43,30 +43,7 @@ struct CourseDetailView: View {
                 }
                 .padding(.bottom, 24)
                 
-                HStack(alignment: .center) {
-                    Image(.locationGreen)
-                    Text("강남구 역삼동")
-                        .font(.body_14_m)
-                        .foregroundColor(.gray400)
-                }
-                .padding(.bottom, 10)
-                
-                HStack(alignment: .center) {
-                    Image(.clockGreen)
-                    Text("2025.07.08(화) | 오후 11:28")
-                        .font(.body_14_m)
-                        .foregroundColor(.gray400)
-                }
-                .padding(.bottom, 12)
-                
-                Text("옵션")
-                    .font(.body_14_m)
-                    .foregroundStyle(.gray200)
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 4)
-                    .background(.pawkeyWhite2)
-                    .cornerRadius(4)
-                    .padding(.bottom, 12)
+                LocationDateView()
                 
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 6) {
