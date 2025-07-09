d54f84934d61747c69ac4a6d29950e19073820ea
KooWihC <ahnchiwook@hanmail.net>
2025-07-10 06:44:37 +0900

    [Feat] #47 반려견 프로필 View 구현

--- PAWKEY/PAWKEY/Presentation/MyPage/View/PetProfileView.swift
+++ PAWKEY/PAWKEY/Presentation/MyPage/View/PetProfileView.swift
@@ -8,7 +8,115 @@
 import SwiftUI
 
 struct PetProfileView: View {
+    @EnvironmentObject var router: TabRouter<MyPageScreen>
+    
     var body: some View {
-        Text("반려견 프로필")
+        VStack(spacing: 8) {
+            Image(.profile)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 108, height: 108)
+                .clipShape(Circle())
+                .padding(.vertical, 36)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                // 이름
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Text("이름")
+                            .font(.body_14_m)
+                            .padding(.leading, 16)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("삼색이")
+                            .font(.head_18_sb)
+                            .foregroundStyle(Color.green500)
+                            .padding(.leading, 16)
+                    }
+                }
+                
+                // 성별
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("성별")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    Text("여아")
+                        .font(.head_18_sb)
+                        .foregroundStyle(Color.green500)
+                        .padding(.leading, 16)
+                }
+                
+                // 중성화
+                Text("중성화했어요")
+                    .font(.caption_12_sb)
+                    .foregroundStyle(Color.gray300)
+                    .padding(.leading, 16)
+                
+                // 견종
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("견종")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    Text("길냥이")
+                        .font(.head_18_sb)
+                        .foregroundStyle(Color.green500)
+                        .padding(.leading, 16)
+                }
+                
+                // 나이
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("나이")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    Text("12세")
+                        .font(.head_18_sb)
+                        .foregroundStyle(Color.green500)
+                        .padding(.leading, 16)
+                }
+                
+                // 성향
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("성향")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    HStack {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("에너지 레벨")
+                                .font(.body_14_sb)
+                                .foregroundStyle(Color.gray400)
+                            Text("활동적이에요")
+                                .font(.head_18_sb)
+                                .foregroundStyle(Color.green500)
+                        }
+                        .padding(.leading, 16)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("사회성 레벨")
+                                .font(.body_14_sb)
+                                .foregroundStyle(Color.gray400)
+                            Text("낯을 가려요")
+                                .font(.head_18_sb)
+                                .foregroundStyle(Color.green500)
+                        }
+                        .padding(.leading, 32)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                Spacer()
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .topNavigationView {
+            Button {
+                router.pop()
+            } label: {
+                Text("스꾸피~")
+            }
+            
+        } center: {
+            Text("반려견 프로필")
+        }
     }
 }
