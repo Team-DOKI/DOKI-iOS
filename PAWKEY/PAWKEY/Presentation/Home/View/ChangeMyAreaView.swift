de64dbd61ed558717bb9141e7a97563ebe012117
sem-git <lsmsm0327@naver.com>
2025-07-10 21:44:53 +0900

    [Feat] #54 ChangeMyAreaView UI 구현

--- PAWKEY/PAWKEY/Presentation/Home/View/ChangeMyAreaView.swift
+++ PAWKEY/PAWKEY/Presentation/Home/View/ChangeMyAreaView.swift
@@ -8,7 +8,72 @@
 import SwiftUI
 
 struct ChangeMyAreaView: View {
+    @EnvironmentObject var router: TabRouter<HomeScreen>
+    
+    @ObservedObject var viewModel: ChangeMyAreaViewModel
+    
+    @EnvironmentObject var tabBarState: TabBarState
+    
     var body: some View {
-        Text("동네 변경 화면")
+        GeometryReader { proxy in
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading) {
+                    Text("지역구")
+                        .font(.body_14_sb)
+                    FlexibleGridView(availableWidth: proxy.size.width - 32,
+                                     data: viewModel.regionList,
+                                     spacing: 14,
+                                     alignment: .leading, content: {
+                        ChoiceButton($0, type: .small, isSelected: $0 == viewModel.userProfile.region) { region in
+                            viewModel.changeUserInfo(.region(region))
+                        }
+                    })
+                    .clipped()
+                }
+                .padding(.top, 28)
+                
+                Spacer().frame(height: 42)
+                
+                if !viewModel.userProfile.region.isEmpty {
+                    VStack(alignment: .leading) {
+                        Text("법정동")
+                            .font(.body_14_sb)
+                        FlexibleGridView(availableWidth: proxy.size.width - 32,
+                                         data: viewModel.legalRegionList,
+                                         spacing: 14,
+                                         alignment: .leading, content: {
+                            ChoiceButton($0, type: .small, isSelected: $0 == viewModel.userProfile.legalRegion) { legalRegion in
+                                viewModel.changeUserInfo(.legalRegion(legalRegion))
+                            }
+                        })
+                        .padding(.trailing, 72)
+                    }
+                }
+                
+                Spacer()
+                
+                CTAButton(
+                    title: "다음으로",
+                    isDisabled: viewModel.userProfile.region.isEmpty || viewModel.userProfile.legalRegion.isEmpty
+                ) {
+                    router.push(.acvitiyAreaMap)
+                }
+                .padding(.bottom, 29)
+            }
+            .padding(.horizontal, 16)
+        }
+        .topNavigationView(left: {
+            BackButton {
+                router.pop()
+            }
+        }, center: {
+            Text("내 동네 설정")
+                .font(.body_16_sb)
+        })
+        .onAppear {
+            withAnimation {
+                tabBarState.isHidden = true
+            }
+        }
     }
 }
