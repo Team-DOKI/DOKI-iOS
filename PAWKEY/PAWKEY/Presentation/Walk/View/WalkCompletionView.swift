06b7e5dacb6deab036ea107e62ecc031a5eb3643
sem-git <lsmsm0327@naver.com>
2025-07-12 01:35:55 +0900

    [Feat] #73 디자인 수정

--- PAWKEY/PAWKEY/Presentation/Walk/View/WalkCompletionView.swift
+++ PAWKEY/PAWKEY/Presentation/Walk/View/WalkCompletionView.swift
@@ -18,53 +18,63 @@ struct WalkCompletionView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("산책 완료!")
-                .font(.head_20_b)
-                .foregroundColor(.pawkeyBlack)
-                .padding(.top, 20)
-                .padding(.bottom, 29)
-            
             Text("포비와 함께한 산책 루트에요.")
                 .font(.head_18_sb)
                 .foregroundColor(.pawkeyBlack)
-                .padding(.bottom, 24)
+                .padding(.top, 36)
+                .padding(.bottom, 16)
+                .padding(.horizontal, 16)
             
-            HStack(spacing: 10) {
-                Circle()
-                    .fill(Color.gray.opacity(0.3))
-                    .frame(width: 43, height: 43)
-                
-                VStack(alignment: .leading, spacing: 6) {
-                    Text("포비")
-                        .font(.body_16_sb)
-                    Text("2025.06.26 (금) | 오후 11:50")
-                        .font(.caption_12_r)
-                        .foregroundColor(.gray300)
+            VStack(alignment: .leading) {
+                HStack(alignment: .center, spacing: 10) {
+                    Circle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 43, height: 43)
+                    
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("포비")
+                            .font(.body_16_sb)
+                        Text("2025.06.26 (금) | 오후 11:50")
+                            .font(.caption_12_r)
+                            .foregroundColor(.gray300)
+                    }
                 }
-                .padding(.bottom, 18)
+                .padding(.horizontal, 16)
+                .padding([.top, .bottom], 12)
+                
+                if let snapshot = snapshot {
+                    Image(uiImage: snapshot)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 218)
+                        .clipped()
+                        .cornerRadius(8)
+                        .padding(.horizontal, 8)
+                        .padding(.bottom, 16)
+                    
+                } else {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.2))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 218)
+                        .cornerRadius(8)
+                        .padding(.horizontal, 8)
+                        .padding(.bottom, 16)
+                }
+                
+                Divider().background(.gray100)
+                
             }
+            .padding(.bottom, 8)
+            .background(.pawkeyWhite1)
+            .cornerRadius(16)
+            .padding(.horizontal, 16)
             
-            if let snapshot = snapshot {
-                Image(uiImage: snapshot)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 156)
-                    .cornerRadius(8)
-                    .padding(.bottom, 10)
-            } else {
-                Rectangle()
-                    .fill(Color.gray.opacity(0.2))
-                    .frame(height: 156)
-                    .cornerRadius(8)
-                    .padding(.bottom, 10)
-            }
-            
-            StatBox(distance: distance, elapsedTime: elapsedTime, stepCount: stepCount)
-              
             Spacer()
             
             CTAButton(
-                title: "산책 기록하기",
+                title: "후기 작성하기",
                 isDisabled: false,
                 buttonStyle: .filled
             ) {
@@ -78,5 +88,10 @@ struct WalkCompletionView: View {
                 tabBarState.isHidden = true
             }
         }
+        .background(.pawkeyWhite2)
+        .topNavigationView(center: {
+            Text("산책 완료")
+                .font(.body_16_sb)
+        })
     }
 }
