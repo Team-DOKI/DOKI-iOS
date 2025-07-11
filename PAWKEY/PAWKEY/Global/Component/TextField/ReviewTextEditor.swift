8267b7945d447da114d0a4310db1946bf60c0ad9
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-12 00:24:01 +0900

    [Refactor] #71 TextField 컴포넌트에서 TextEditor 분리

--- PAWKEY/PAWKEY/Global/Component/TextField/ReviewTextEditor.swift
+++ PAWKEY/PAWKEY/Global/Component/TextField/ReviewTextEditor.swift
@@ -5,3 +5,45 @@
 //  Created by 권석기 on 7/11/25.
 //
 
+import SwiftUI
+
+struct ReviewTextEditor: View {
+    @Binding var text: String
+    
+    @FocusState private var isFocused: Bool
+    
+    var placeholder: String {
+        return "산책 후기를 간단하게 적어주세요!"
+    }
+    
+    var borderColor: Color {
+        text.isEmpty ? .gray200 : .clear
+    }
+    
+    var body: some View {
+        TextEditor(text: $text)
+            .scrollContentBackground(.hidden)
+            .font(.body_14_r)
+            .padding(.leading, 16)
+            .padding(.top, 12)
+            .frame(width: 343, height: 214)
+            .background(Color.clear)
+            .focused($isFocused)
+            .overlay(alignment: .topLeading) {
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(isFocused ? Color.pawkeyBlack : Color.gray50.opacity(0.5), lineWidth: 1)
+                    Text(placeholder)
+                        .foregroundStyle(borderColor)
+                        .font(.body_14_r)
+                        .padding(.leading, 20)
+                        .padding(.top, 20)
+                }
+            }
+            .background(Color.pawkeyWhite2)
+    }
+}
+
+#Preview {
+    ReviewTextEditor(text: .constant(""))
+}
