fd76fb3257b21e577f8979118f4e74961d6b5501
sem-git <lsmsm0327@naver.com>
2025-07-12 02:12:18 +0900

    [Feat] #73 SafeArea ignore으로 배경색 적용

--- PAWKEY/PAWKEY/Presentation/Walk/View/WalkCompletionView.swift
+++ PAWKEY/PAWKEY/Presentation/Walk/View/WalkCompletionView.swift
@@ -17,83 +17,87 @@ struct WalkCompletionView: View {
     let snapshot: UIImage?
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("포비와 함께한 산책 루트에요.")
-                .font(.head_18_sb)
-                .foregroundColor(.pawkeyBlack)
-                .padding(.top, 36)
-                .padding(.bottom, 16)
-                .padding(.horizontal, 16)
+        ZStack {
+            Color.pawkeyWhite2
+                .ignoresSafeArea()
             
             VStack(alignment: .leading) {
-                HStack(alignment: .center, spacing: 10) {
-                    Circle()
-                        .fill(Color.gray.opacity(0.3))
-                        .frame(width: 43, height: 43)
-                    
-                    VStack(alignment: .leading, spacing: 6) {
-                        Text("포비")
-                            .font(.body_16_sb)
-                        Text("2025.06.26 (금) | 오후 11:50")
-                            .font(.caption_12_r)
-                            .foregroundColor(.gray300)
+                Text("포비와 함께한 산책 루트에요.")
+                    .font(.head_18_sb)
+                    .foregroundColor(.pawkeyBlack)
+                    .padding(.top, 36)
+                    .padding(.bottom, 16)
+                    .padding(.horizontal, 16)
+                
+                VStack(alignment: .leading) {
+                    HStack(alignment: .center, spacing: 10) {
+                        Circle()
+                            .fill(Color.gray.opacity(0.3))
+                            .frame(width: 43, height: 43)
+                        
+                        VStack(alignment: .leading, spacing: 6) {
+                            Text("포비")
+                                .font(.body_16_sb)
+                            Text("2025.06.26 (금) | 오후 11:50")
+                                .font(.caption_12_r)
+                                .foregroundColor(.gray300)
+                        }
                     }
+                    .padding(.horizontal, 16)
+                    .padding([.top, .bottom], 12)
+                    
+                    if let snapshot = snapshot {
+                        Image(uiImage: snapshot)
+                            .resizable()
+                            .frame(height: 218)
+                            .frame(maxWidth: .infinity)
+                            .cornerRadius(8)
+                            .padding(.horizontal, 8)
+                            .padding(.bottom, 16)
+                    } else {
+                        Rectangle()
+                            .fill(Color.gray.opacity(0.2))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 218)
+                            .cornerRadius(8)
+                            .padding(.horizontal, 8)
+                            .padding(.bottom, 16)
+                    }
+                    
+                    Divider().background(.gray100)
+                    
+                    StatBox(type: .borderless, distance: distance, elapsedTime: elapsedTime, stepCount: stepCount)
                 }
+                .padding(.bottom, 8)
+                .background(.pawkeyWhite1)
+                .cornerRadius(16)
                 .padding(.horizontal, 16)
-                .padding([.top, .bottom], 12)
                 
-                if let snapshot = snapshot {
-                    Image(uiImage: snapshot)
-                        .resizable()
-                        .frame(height: 218)
-                        .frame(maxWidth: .infinity)
-                        .cornerRadius(8)
-                        .padding(.horizontal, 8)
-                        .padding(.bottom, 16)
-                } else {
-                    Rectangle()
-                        .fill(Color.gray.opacity(0.2))
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 218)
-                        .cornerRadius(8)
-                        .padding(.horizontal, 8)
-                        .padding(.bottom, 16)
+                Spacer()
+                
+                CTAButton(
+                    title: "후기 작성하기",
+                    isDisabled: false,
+                    buttonStyle: .filled
+                ) {
+                    router.push(.archive(snapshot: snapshot))
                 }
-                
-                Divider().background(.gray100)
-                
-                StatBox(type: .borderless, distance: distance, elapsedTime: elapsedTime, stepCount: stepCount)
+                .padding(.bottom, 26)
             }
-            .padding(.bottom, 8)
-            .background(.pawkeyWhite1)
-            .cornerRadius(16)
             .padding(.horizontal, 16)
-            
-            Spacer()
-            
-            CTAButton(
-                title: "후기 작성하기",
-                isDisabled: false,
-                buttonStyle: .filled
-            ) {
-                router.push(.archive(snapshot: snapshot))
+            .onAppear {
+                withAnimation {
+                    tabBarState.isHidden = true
+                }
             }
-            .padding(.bottom, 26)
+            .topNavigationView(left: {
+                BackButton {
+                    router.pop()
+                }
+            }, center: {
+                Text("산책 완료")
+                    .font(.body_16_sb)
+            })
         }
-        .padding(.horizontal, 16)
-        .onAppear {
-            withAnimation {
-                tabBarState.isHidden = true
-            }
-        }
-        .background(.pawkeyWhite2)
-        .topNavigationView(left: {
-            BackButton {
-                router.pop()
-            }
-        }, center: {
-            Text("산책 완료")
-                .font(.body_16_sb)
-        })
     }
 }
