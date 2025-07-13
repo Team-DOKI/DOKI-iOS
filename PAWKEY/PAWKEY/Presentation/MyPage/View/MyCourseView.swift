0d08547781c97f1a25e63715310074d43686ea77
KooWihC <ahnchiwook@hanmail.net>
2025-07-14 02:05:44 +0900

    [Feat] #82 CourseCard를 터치 했을 시 -> push CourseDetailView 구현

--- PAWKEY/PAWKEY/Presentation/MyPage/View/MyCourseView.swift
+++ PAWKEY/PAWKEY/Presentation/MyPage/View/MyCourseView.swift
@@ -7,19 +7,47 @@
 
 import SwiftUI
 
+struct Review: Identifiable {
+    let id = UUID()
+    let walkRouteImg: String
+    let profileImg: String
+    let walkTitle: String
+    let petName: String
+    let postDate: String
+    var buttonPressed: Bool
+}
+
 struct MyCourseView: View {
     @EnvironmentObject var router: Coordinator<MyPageScreen>
-    let dummyData = ["이륜차 거의 없음", "배변 쓰레기통", "쉼터", "편의점", "동반 카페", "아스팔트/벽돌", "시끌벅적"]
+    let tagList = ["이륜차 거의 없음", "배변 쓰레기통", "쉼터", "편의점", "동반 카페", "아스팔트/벽돌", "시끌벅적"]
+    
+    let myReviewList: [Review] = [
+        Review(walkRouteImg: "walkRoute", profileImg: "profile6", walkTitle: "아시는구나~", petName: "포키", postDate: "2025/01/02", buttonPressed: true),
+        Review(walkRouteImg: "walkRoute2", profileImg: "profile2", walkTitle: "신나는 산책", petName: "쮸비", postDate: "2025/03/04", buttonPressed: false),
+        Review(walkRouteImg: "walkRoute3", profileImg: "profile3", walkTitle: "더운 산책", petName: "수민이누나", postDate: "2025/05/06", buttonPressed: true),
+        Review(walkRouteImg: "walkRoute", profileImg: "profile4", walkTitle: "스꾸삐~", petName: "세민이", postDate: "2025/07/08", buttonPressed: false),
+        Review(walkRouteImg: "walkRoute2", profileImg: "profile5", walkTitle: "그냥 산책", petName: "치즈", postDate: "2025/09/10", buttonPressed: false)
+    ]
     
     var body: some View {
         VStack {
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 16) {
-                    ReviewCard(type: .mine, walkRouteImg: "walkRoute", profileImg: "profile6", walkTitle: "아시는구나~", petName: "포키", postDate: "2025/01/02", buttonPressed: true, data: dummyData)
-                    ReviewCard(type: .mine, walkRouteImg: "walkRoute2", profileImg: "profile2", walkTitle: "신나는 산책", petName: "쮸비", postDate: "2025/03/04", buttonPressed: false, data: dummyData)
-                    ReviewCard(type: .mine, walkRouteImg: "walkRoute3", profileImg: "profile3", walkTitle: "더운 산책", petName: "수민이누나", postDate: "2025/05/06", buttonPressed: true, data: dummyData)
-                    ReviewCard(type: .mine, walkRouteImg: "walkRoute", profileImg: "profile4", walkTitle: "스꾸삐~", petName: "세민이", postDate: "2025/07/08", buttonPressed: false, data: dummyData)
-                    ReviewCard(type: .mine, walkRouteImg: "walkRoute2", profileImg: "profile5", walkTitle: "그냥 산책", petName: "치즈", postDate: "2025/09/10", buttonPressed: false, data: dummyData)
+                    ForEach(myReviewList, id: \.id) { review in
+                        ReviewCard(
+                            type: .mine,
+                            walkRouteImg: review.walkRouteImg,
+                            profileImg: review.profileImg,
+                            walkTitle: review.walkTitle,
+                            petName: review.petName,
+                            postDate: review.postDate,
+                            buttonPressed: review.buttonPressed,
+                            data: tagList
+                        )
+                        .onTapGesture {
+                            router.push(.courseDetail)
+                        }
+                    }
                 }
                 .padding(.top, 12)
                 .padding(.bottom, 108)
@@ -36,6 +64,7 @@ struct MyCourseView: View {
                 .font(.body_16_sb)
         })
         .ignoresSafeArea(edges: .bottom)
-
+        
     }
 }
+
