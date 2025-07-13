d3c2989edf9476d59f9714d3c37c8519721d6ea9
sem-git <lsmsm0327@naver.com>
2025-07-14 01:59:23 +0900

    [Feat] #84 공유된 루트 산책 UI 및 종료 로직 구현

--- PAWKEY/PAWKEY/Presentation/SharedWalk/View/SharedWalkCourseView.swift
+++ PAWKEY/PAWKEY/Presentation/SharedWalk/View/SharedWalkCourseView.swift
@@ -9,13 +9,90 @@ import SwiftUI
 
 struct SharedWalkCourseView: View {
     @EnvironmentObject var router: Coordinator<WalkScreen>
-    let sharedCourseId: Int
+    
+    @ObservedObject var viewModel: SharedWalkCourseViewModel
+    
+    @Binding var showSharedWalkCourseView: Bool
+    
+    @State private var showStopConfirmation = false
+    
+    let onComplete: (Double, String, Int, UIImage?) -> Void
     
     var body: some View {
-        Text("공유 산책 경로: \(sharedCourseId)")
-        
-        Button("공유 산책 완료") {
-            router.push(.sharedWalkCompletion(id: sharedCourseId))
+        ZStack {
+            ZStack {
+                SharedWalkMap(
+                    region: $viewModel.region,
+                    shouldCenterOnUser: $viewModel.shouldCenterOnUser,
+                    pathCoordinates: viewModel.examplePathCoordinates
+                )
+                .edgesIgnoringSafeArea(.all)
+                
+                if showStopConfirmation {
+                    Color.pawkeyBlack.opacity(0.5)
+                        .edgesIgnoringSafeArea(.all)
+                }
+            }
+            
+            VStack {
+                StatBox(type: .bordered, distance: viewModel.distance, elapsedTime: viewModel.elapsedTime, stepCount: viewModel.stepCount)
+                    .padding(.top, 24)
+                    .padding(.horizontal, 16)
+                
+                Spacer()
+                
+                if showStopConfirmation {
+                    StopConfirmationView(
+                        onResume: {
+                            showStopConfirmation = false
+                            viewModel.setPaused(false)
+                        },
+                        onStop: {
+                            viewModel.stopTracking()
+                            showSharedWalkCourseView = false
+                            onComplete(
+                                viewModel.distance,
+                                viewModel.elapsedTime,
+                                viewModel.stepCount,
+                                nil
+                            )
+                        }
+                    )
+                } else {
+                    Spacer()
+                    
+                    CTAButton(
+                        title: "종료하기",
+                        isDisabled: false,
+                        buttonStyle: .filled
+                    ) {
+                        showStopConfirmation = true
+                        viewModel.setPaused(true)
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 26)
+                }
+            }
+            
+            if !showStopConfirmation {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            viewModel.shouldCenterOnUser = true
+                            viewModel.centerMapOnCurrentLocation()
+                        }) {
+                            Image(.mapGps)
+                        }
+                        .padding(.trailing, 16)
+                        .padding(.bottom, 102)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            viewModel.startTracking()
         }
     }
 }
