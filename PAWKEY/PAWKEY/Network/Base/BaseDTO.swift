76d845fdd42f7ca01dd21548bd516459e202cc01
sem-git <lsmsm0327@naver.com>
2025-07-15 02:49:24 +0900

    [Network] #99 BaseDTO 추가

---
+++ PAWKEY/PAWKEY/Network/Base/BaseDTO.swift
@@ -0,0 +1,16 @@
+//
+//  BaseDTO.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 6/21/25.
+//
+
+import Foundation
+
+struct BaseDTO<T: Codable>: Codable {
+    let code: String
+    let message: String
+    let data: T?
+}
+
+struct EmptyDTO: Codable {}
