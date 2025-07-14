bda311e7f9b4d463f41b1af2838c18a365bb30a0
sem-git <lsmsm0327@naver.com>
2025-07-15 02:51:40 +0900

    [Network] #99 ActivityAreaMapDTO 추가

---
+++ PAWKEY/PAWKEY/Network/ActivityArea/ActivityAreaMapDTO.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityAreaMapDTO.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/14/25.
+//
+
+import Foundation
+
+struct RegionDTO: Codable {
+    let regionName: String
+    let geometryDto: GeometryDTO
+}
+
+struct GeometryDTO: Codable {
+    let type: String
+    let coordinates: [[[[Double]]]]
+}
