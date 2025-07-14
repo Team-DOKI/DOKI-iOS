b03feaeece5a06f15f49a4d7a2bfc5ac6238fd71
sem-git <lsmsm0327@naver.com>
2025-07-15 06:28:45 +0900

    [Network] #102 ActivityAreaMapView 기존 산책하던 지역 바인딩

--- PAWKEY/PAWKEY/Presentation/ActivityArea/ViewModel/ActivityAreaMapViewModel.swift
+++ PAWKEY/PAWKEY/Presentation/ActivityArea/ViewModel/ActivityAreaMapViewModel.swift
@@ -8,18 +8,21 @@
 import MapKit
 import Moya
 
-class ActivityAreaMapViewModel: ObservableObject {    
+
+class ActivityAreaMapViewModel: ObservableObject {
     @Published var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.5215, longitude: 127.0250),
         span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
     )
     @Published var activityAreas: [ActivityArea] = []
+    @Published var preRegionName: String = ""
     @Published var regionName: String = ""
     @Published var errorMessage: String?
     @Published var isShowToast = false
     
     private let provider = MoyaProvider<ActivityAreaMapAPI>()
     
+    @MainActor
     func fetchActivityArea(regionId: Int) async {
         do {
             let response: BaseDTO<RegionDTO> = try await provider.async.request(.fetchCoordinates(regionId: regionId))
@@ -28,8 +31,9 @@ class ActivityAreaMapViewModel: ObservableObject {
                 errorMessage = "데이터 없음"
                 return
             }
-            print("\(response.message): \(data.regionName)")
+            print("\(response.message): \(data.regionName) 조회, 기존 산책 지역: \(data.preRegionName)")
             
+            self.preRegionName = data.preRegionName.components(separatedBy: " ").last ?? data.preRegionName
             self.regionName = data.regionName.components(separatedBy: " ").last ?? data.regionName
             
             let coords = data.geometryDto.coordinates.map { polygon in
