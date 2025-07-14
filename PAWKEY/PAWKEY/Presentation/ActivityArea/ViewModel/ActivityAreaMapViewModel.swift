1061a4b4360c2b21dfa4122da1739e3adad6c1d1
sem-git <lsmsm0327@naver.com>
2025-07-15 02:54:26 +0900

    [Network] #99 사용자 지역 표시 및 변경 메서드 추가

---
+++ PAWKEY/PAWKEY/Presentation/ActivityArea/ViewModel/ActivityAreaMapViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  ActivityAreaMapViewModel.swift
+//  PAWKEY
+//
+//  Created by 이세민 on 7/15/25.
+//
+
+import MapKit
+import Moya
+
+@MainActor
+class ActivityAreaMapViewModel: ObservableObject {    
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.5215, longitude: 127.0250),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    @Published var activityAreas: [ActivityArea] = []
+    @Published var regionName: String = ""
+    @Published var errorMessage: String?
+    
+    private let provider = MoyaProvider<ActivityAreaMapAPI>()
+    
+    func fetchActivityArea(regionId: Int) async {
+        do {
+            let response: BaseDTO<RegionDTO> = try await provider.async.request(.fetchCoordinates(regionId: regionId))
+            
+            guard let data = response.data else {
+                errorMessage = "데이터 없음"
+                return
+            }
+            print("\(response.message): \(data.regionName)")
+            
+            self.regionName = data.regionName.components(separatedBy: " ").last ?? data.regionName
+            
+            let coords = data.geometryDto.coordinates.map { polygon in
+                polygon.map { ring in
+                    ring.map { point in
+                        CLLocationCoordinate2D(latitude: point[1], longitude: point[0])
+                    }
+                }
+            }
+            
+            activityAreas = coords.map { ActivityArea(coordinates: $0) }
+            
+            if let first = coords.first?.first?.first {
+                region.center = first
+            }
+        } catch {
+            errorMessage = "에러 발생: \(error.localizedDescription)"
+        }
+    }
+    
+    func updateUserRegion(regionId: Int) async {
+        do {
+            let response: BaseDTO<EmptyDTO> = try await provider.async.request(.updateUserRegion(regionId: regionId))
+            
+            print(response.message)
+        } catch {
+            errorMessage = "지역 변경 실패: \(error.localizedDescription)"
+        }
+    }
+}
