b2474bfdd2c259cea6e93ffb33090d8fb218727c
KooWihC <ahnchiwook@hanmail.net>
2025-07-16 08:23:30 +0900

    [Network] #125 UserProfile API 연결

--- PAWKEY/PAWKEY/Presentation/MyPage/View/UserProfileView.swift
+++ PAWKEY/PAWKEY/Presentation/MyPage/View/UserProfileView.swift
@@ -11,58 +11,63 @@ struct UserProfileView: View {
     @EnvironmentObject var coordinator: Coordinator<MyPageScene>
     @EnvironmentObject var mainTabViewModel: MainTabViewModel
     
+    @StateObject var viewModel: UserProfileViewModel
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            // 이름
-            VStack(alignment: .leading, spacing: 10) {
-                HStack {
-                    Text("이름")
+            if let user = viewModel.userProfile {
+                // 이름
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Text("이름")
+                            .font(.body_14_m)
+                            .padding(.leading, 16)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(user.name)
+                            .font(.head_18_sb)
+                            .foregroundStyle(Color.green500)
+                            .padding(.leading, 16)
+                    }
+                }
+                
+                // 성별
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("성별")
                         .font(.body_14_m)
                         .padding(.leading, 16)
-                    Spacer()
-                }
-                HStack {
-                    Text("김도기")
+                    Text(user.gender)
                         .font(.head_18_sb)
                         .foregroundStyle(Color.green500)
                         .padding(.leading, 16)
                 }
+                
+                // 나이
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("나이")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    Text("\(user.age)세")
+                        .font(.head_18_sb)
+                        .foregroundStyle(Color.green500)
+                        .padding(.leading, 16)
+                }
+                
+                // 활동지역
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("활동지역")
+                        .font(.body_14_m)
+                        .padding(.leading, 16)
+                    Text(user.region)
+                        .font(.head_18_sb)
+                        .foregroundStyle(Color.green500)
+                        .padding(.leading, 16)
+                }
+                
+                Spacer()
             }
             
-            // 성별
-            VStack(alignment: .leading, spacing: 10) {
-                Text("성별")
-                    .font(.body_14_m)
-                    .padding(.leading, 16)
-                Text("여성")
-                    .font(.head_18_sb)
-                    .foregroundStyle(Color.green500)
-                    .padding(.leading, 16)
-            }
-            
-            // 나이
-            VStack(alignment: .leading, spacing: 10) {
-                Text("나이")
-                    .font(.body_14_m)
-                    .padding(.leading, 16)
-                Text("24세")
-                    .font(.head_18_sb)
-                    .foregroundStyle(Color.green500)
-                    .padding(.leading, 16)
-            }
-            
-            // 활동지역
-            VStack(alignment: .leading, spacing: 10) {
-                Text("활동지역")
-                    .font(.body_14_m)
-                    .padding(.leading, 16)
-                Text("강남구 역삼동")
-                    .font(.head_18_sb)
-                    .foregroundStyle(Color.green500)
-                    .padding(.leading, 16)
-            }
-            
-            Spacer()
         }
         .navigationBarBackButtonHidden()
         .topNavigationView(left: {
@@ -74,6 +79,11 @@ struct UserProfileView: View {
             Text("견주 프로필")
                 .font(.body_16_sb)
         })
+        .onAppear {
+            Task {
+                await viewModel.fetchUserProfile()
+            }
+        }
         
     }
 }
