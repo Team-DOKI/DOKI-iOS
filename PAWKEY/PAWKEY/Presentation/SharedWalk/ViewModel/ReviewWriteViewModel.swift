5bde7f35fcd2907ab650e20b017efb9c3c41b51b
sem-git <lsmsm0327@naver.com>
2025-07-16 07:38:12 +0900

    [Network] #127 fetchCourseCategories 메서드 구현

--- PAWKEY/PAWKEY/Presentation/SharedWalk/ViewModel/ReviewWriteViewModel.swift
+++ PAWKEY/PAWKEY/Presentation/SharedWalk/ViewModel/ReviewWriteViewModel.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import Moya
 
 class ReviewWriteViewModel: ObservableObject {
-    @Published var safetyTags: Set<String> = []
-    @Published var facilityTags: Set<String> = []
+    private let provider = MoyaProvider<ReviewWriteAPI>()
+    
+    @Published var categories: [CategoryList] = []
+    @Published var selectedOptions: [Int: Set<String>] = [:]
+    
     
     var isButtonDisabled: Bool {
-        !safetyTags.isEmpty &&
-        !facilityTags.isEmpty
+        selectedOptions.values.allSatisfy { !$0.isEmpty }
+    }
+    
+    @MainActor
+    func fetchCourseCategories() async {
+        do {
+            let response: BaseDTO<CategoryDTO> = try await provider.async.request(ReviewWriteAPI.fetchCourseCategories)
+            
+            if let data = response.data {
+                self.categories = data.categoryList
+                for category in data.categoryList {
+                    selectedOptions[category.categoryId] = []
+                }
+                
+                print("\(response.message)")
+            }
+        } catch {
+            print("카테고리 불러오기 실패: \(error)")
+        }
+    }
+    
+    func categoryOptionTexts(_ categoryId: Int) -> [String] {
+        categories.first(where: { $0.categoryId == categoryId })?.categoryOptions.map { $0.categoryOptionText } ?? []
+    }
+    
+    func selectedOptionsBinding(_ categoryId: Int) -> Binding<Set<String>> {
+        Binding(
+            get: { self.selectedOptions[categoryId] ?? [] },
+            set: { self.selectedOptions[categoryId] = $0 }
+        )
     }
 }
