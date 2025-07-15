eed0bdc1a84cc6700e8e5f1c699f70f4d3709819
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-15 21:17:01 +0900

    [Feat] #110 지역구리스트 API 연동 및 데이터 저장

--- PAWKEY/PAWKEY/Network/Region/RegionDTO.swift
+++ PAWKEY/PAWKEY/Network/Region/RegionDTO.swift
@@ -20,3 +20,21 @@ struct AreaDTO: Codable {
     let id: Int
     let name: String
 }
+
+extension Array where Element == DistrictUnitDTO {
+    func toEntity() -> [RegionUnit] {
+        map { RegionUnit(gu: $0.gu.toEntity(), dong: $0.dongs.toEntity())}
+    }
+}
+
+extension AreaDTO {
+    func toEntity() -> Area {
+        Area(id: id, name: name)
+    }
+}
+
+extension Array where Element == AreaDTO {
+    func toEntity() -> [Area] {
+        map { Area(id: $0.id, name: $0.name )}
+    }
+}
