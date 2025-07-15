9f3194e4771fb5ef3a2eea54f3663d5da5bd8574
kwonseokki <kwons3327@gmail.com>
2025-07-15 11:37:32 +0900

    [Feat] #105 반려견 성향 API 연동

--- PAWKEY/PAWKEY/Network/PetTrait/PetTraitsCategoryDTO.swift
+++ PAWKEY/PAWKEY/Network/PetTrait/PetTraitsCategoryDTO.swift
@@ -7,17 +7,41 @@
 
 import Foundation
 
-struct PetTraitData: Decodable {
-    let petTraitCategoryList: [PetTraitCategory]
+struct PetTraitDTO: Codable {
+    let petTraitCategoryList: [PetTraitCategoryDTO]
 }
 
-struct PetTraitCategory: Decodable {
+struct PetTraitCategoryDTO: Codable {
     let petTraitCategoryId: Int
     let petTraitCategoryName: String
-    let petTraitCategoryOptions: [PetTraitCategoryOption]
+    let petTraitCategoryOptions: [PetTraitCategoryOptionDTO]
 }
 
-struct PetTraitCategoryOption: Decodable {
+struct PetTraitCategoryOptionDTO: Codable {
     let petTraitCategoryOptionId: Int
     let petTraitCategoryOptionText: String
 }
+
+extension PetTraitCategoryDTO {
+    func toEntity() -> PetTraitCategory {
+        PetTraitCategory(
+            categoryId: petTraitCategoryId,
+            categoryName: petTraitCategoryName,
+            categoryOptions: petTraitCategoryOptions.toEntity()
+        )
+    }
+}
+
+extension Array where Element == PetTraitCategoryOptionDTO  {
+    func toEntity() -> [PetTraitCategoryOption] {
+        map {
+            PetTraitCategoryOption(
+                categoryOptionId: $0.petTraitCategoryOptionId,
+                categoryOptionText: $0.petTraitCategoryOptionText
+            )
+        }
+    }
+}
+
+
+
