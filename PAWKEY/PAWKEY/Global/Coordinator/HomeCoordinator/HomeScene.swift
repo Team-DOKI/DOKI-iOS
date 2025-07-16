2651198a9785c75f0fa8841357d8740dcbd65015
sem-git <lsmsm0327@naver.com>
2025-07-17 04:11:41 +0900

    [Feat] #145 regionId 파라미터 추가

--- PAWKEY/PAWKEY/Global/Coordinator/HomeCoordinator/HomeScene.swift
+++ PAWKEY/PAWKEY/Global/Coordinator/HomeCoordinator/HomeScene.swift
@@ -10,7 +10,7 @@ import SwiftUI
 enum HomeScene: AppScene {
     case home
     case changeMyArea
-    case acvitiyAreaMap
+    case acvitiyAreaMap(regionId: Int)
     case sharedCourseDetail(id: Int)
     
     @ViewBuilder
@@ -19,11 +19,14 @@ enum HomeScene: AppScene {
         case .home:
             let viewModel = HomeViewModel()
             HomeView(viewModel: viewModel)
+            
         case .changeMyArea:
             ChangeActivityAreaView(viewModel: ChangeActivityAreaViewModel())
-        case .acvitiyAreaMap:
-            let viewModel = ActivityAreaMapViewModel()
+            
+        case .acvitiyAreaMap(let regionId):
+            let viewModel = ActivityAreaMapViewModel(regionId: regionId)
             ActivityAreaMapView(viewModel: viewModel)
+            
         case let .sharedCourseDetail(id):
             let viewModel = CourseDetailViewModel(postId: id)
             CourseDetailView(viewModel: viewModel)
