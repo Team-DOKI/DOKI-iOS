f12c1ef0c6a75db65a371054bfabc05044722891
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-16 11:17:20 +0900

    [Fix] #109 핉터 API 연동 및 뷰모델 더미데이터 제거

--- PAWKEY/PAWKEY/Presentation/MapAndList/ViewModel/MapAndListViewModel.swift
+++ PAWKEY/PAWKEY/Presentation/MapAndList/ViewModel/MapAndListViewModel.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import Moya
+
 final class MapAndListViewModel: ObservableObject {
     
     enum WalkRouteOption {
@@ -18,81 +20,12 @@ final class MapAndListViewModel: ObservableObject {
     }
     
     @Published var isShowSheet = false
-    @Published var isExpandWalkingTime = false
-    @Published var isExpandSafety = false
-    @Published var isExpandConvenience = false
-    @Published var isExpandEnvironment = false
-    @Published var isExpandMood = false
-    
-    @Published var walkingTimeList: [CheckItem] = [
-        .init(title: "20분 이내", isSelected: false),
-        .init(title: "21~40분", isSelected: false),
-        .init(title: "41~60분", isSelected: false),
-        .init(title: "1시간 넘게", isSelected: false),
-    ]
-    
-    @Published var safetyList: [CheckItem] = [
-        .init(title: "킥보드/자전거 거의 없음", isSelected: false),
-        .init(title: "차량 거의 없음", isSelected: false),
-        .init(title: "야간에도 밝음", isSelected: false),
-        .init(title: "보도/차도 구분됨", isSelected: false),
-        .init(title: "넓은 보도", isSelected: false),
-    ]
-    
-    @Published var convenienceList: [CheckItem] = [
-        .init(title: "배변 봉투 쓰레기통", isSelected: false),
-        .init(title: "벤치", isSelected: false),
-        .init(title: "편의점", isSelected: false),
-        .init(title: "반려견 동반 카페", isSelected: false),
-    ]
-    
-    @Published var environmentList: [CheckItem] = [
-        .init(title: "풀 많은 길 위주", isSelected: false),
-        .init(title: "흙길 위주", isSelected: false),
-        .init(title: "아스팔트/벽돌길 위주", isSelected: false),
-        .init(title: "뛰어놀 공간", isSelected: false),
-    ]
-    
-    @Published var moodList: [CheckItem] = [
-        .init(title: "조용한 분위기", isSelected: false),
-        .init(title: "적당한 유동인구", isSelected: false),
-        .init(title: "유동 인구 많음", isSelected: false),
-    ]
-    
-    var selectedOptions: [CheckItem] {
-        return walkingTimeList.filter { $0.isSelected } +
-        safetyList.filter { $0.isSelected } +
-        convenienceList.filter { $0.isSelected } +
-        environmentList.filter { $0.isSelected } +
-        moodList.filter { $0.isSelected }
-    }
-    
-    func selectWalkRouteOption(_ option: WalkRouteOption) {
-        switch option {
-        case .walkingTime(let index):
-            walkingTimeList = selecteMultipleOption(at: index, from: walkingTimeList)
-        case .safety(let index):
-            safetyList = selecteMultipleOption(at: index, from: safetyList)
-        case .convenience(let index):
-            convenienceList = selecteMultipleOption(at: index, from: convenienceList)
-        case .environment(let index):
-            environmentList = selecteSingleOption(at: index, from: environmentList)
-        case .mood(let index):
-            moodList = selecteSingleOption(at: index, from: moodList)
-        }
-    }
+    @Published var filterItemList = FilterList()
+    @Published var expandedGroup: [Int: Bool] = [:]
+    @Published var selectedFilterItem: [SelecteItem] = []
     
     func resetAllOptions() {
-        walkingTimeList = walkingTimeList.map { .init(title: $0.title, isSelected: false)}
-        safetyList = safetyList.map { .init(title: $0.title, isSelected: false)}
-        convenienceList = convenienceList.map { .init(title: $0.title, isSelected: false)}
-        environmentList = environmentList.map { .init(title: $0.title, isSelected: false)}
-        moodList = moodList.map { .init(title: $0.title, isSelected: false)}
-        isExpandWalkingTime = false
-        isExpandSafety = false
-        isExpandConvenience = false
-        isExpandEnvironment = false
-        isExpandMood = false
+     
     }
     
     private func selecteSingleOption(at index: Int, from list: [CheckItem]) -> [CheckItem] {
@@ -112,5 +45,30 @@ final class MapAndListViewModel: ObservableObject {
         var newList = list
         newList[index].isSelected.toggle()
         return newList
+    }
+}
+
+// MARK: - API
+
+extension MapAndListViewModel {
+    
+    @MainActor
+    func fetchFilterOptions() async {
+        let provider = MoyaProvider<FilterAPI>(plugins: [MoyaLoggingPlugin()])
+        do {
+            let response: BaseDTO<FilterDTO> = try await provider.async.request(.fetchFilterOptions)
+            
+            guard let data = response.data else {
+                return
+            }
+            
+            let selectList = data.selectList.toEntity()
+            let categoryList = data.categoryList.toEntity()
+            
+            filterItemList.selecteList = selectList + Array(categoryList.prefix(2))
+            filterItemList.categoryList = Array(categoryList.dropFirst(2))
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
