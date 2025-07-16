651c35d3f2dec37e015f090376f269c2f3bb0110
kwonseokki <kwons3327@gmail.com>
2025-07-16 20:29:04 +0900

    [Fix] #109 강제로 채번하는 코드 제거

--- PAWKEY/PAWKEY/Network/Filtering/FilterDataMapper.swift
+++ PAWKEY/PAWKEY/Network/Filtering/FilterDataMapper.swift
@@ -8,42 +8,24 @@
 import Foundation
 
 extension Array where Element == SelectListDTO {
-    func toEntity(startingId: Int = 1) -> [SelecteList] {
-        var currentId = startingId
-        return self.map { dto in
-            let newOptions = dto.options.enumerated().map { idx, opt in
-                SelecteItem(
-                    selectOptionId: currentId * 100 + idx,
-                    selectText: opt.selectText,
-                    isSelected: false
-                )
-            }
-            defer { currentId += 1 }
-            return SelecteList(
-                selectId: currentId,
-                selectName: dto.selectName,
-                options: newOptions
+    func toEntity() -> [SelectList] {
+        map {
+            SelectList(
+                selectId: $0.selectId,
+                selectName: $0.selectName,
+                options: $0.options.toEntity()
             )
         }
     }
 }
 
 extension Array where Element == CategoryListDTO {
-    func toEntity(startingId: Int = 100) -> [SelecteList] {
-        var currentId = startingId
-        return self.map { dto in
-            let newOptions = dto.options.enumerated().map { idx, opt in
-                SelecteItem(
-                    selectOptionId: currentId * 100 + idx,
-                    selectText: opt.optionText,
-                    isSelected: false
-                )
-            }
-            defer { currentId += 1 }
-            return SelecteList(
-                selectId: currentId,
-                selectName: dto.categoryName,
-                options: newOptions
+    func toEntity() -> [SelectList] {
+        map {
+            SelectList(
+                selectId: $0.categoryId,
+                selectName: $0.categoryName,
+                options: $0.options.toEntity()
             )
         }
     }
@@ -51,8 +33,8 @@ extension Array where Element == CategoryListDTO {
 
 
 extension SelectListDTO {
-    func toEntity() -> SelecteList {
-        SelecteList(
+    func toEntity() -> SelectList {
+        SelectList(
             selectId: selectId,
             selectName: selectName,
             options: options.toEntity()
@@ -72,8 +54,8 @@ extension Array where Element == SelectItemDTO {
 }
 
 extension CategoryListDTO {
-    func toEntity() -> SelecteList {
-        SelecteList(
+    func toEntity() -> SelectList {
+        SelectList(
             selectId: categoryId,
             selectName: categoryName,
             options: options.toEntity()
