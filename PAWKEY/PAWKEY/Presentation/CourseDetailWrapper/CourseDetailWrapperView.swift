51f8f7bb340f2f6af4a32eb3411224059f544321
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-07-17 21:45:26 +0900

    [Addt] #156 Course Detail WrapperView 추가

---
+++ PAWKEY/PAWKEY/Presentation/CourseDetailWrapper/CourseDetailWrapperView.swift
@@ -0,0 +1,36 @@
+//
+//  CourseDetailWrapperView.swift
+//  PAWKEY
+//
+//  Created by 권석기 on 7/17/25.
+//
+
+import SwiftUI
+
+struct CourseDetailWrapperView: View {
+    @EnvironmentObject var coordinator: Coordinator<WalkScene>
+    @EnvironmentObject var mainTabViewModel: MainTabViewModel
+    @ObservedObject var viewModel: CourseDetailViewModel
+    
+    var body: some View {
+        VStack (spacing: 0) {
+            CourseDetailView(viewModel: viewModel) {
+                coordinator.popToRoot()
+                mainTabViewModel.isHidden = false
+            }
+            .overlay(alignment: .bottom) {
+                    Button(action: {
+                        coordinator.popToRoot()
+                    }) {
+                        Text("해당 루트로 산책하기")
+                            .font(.body_16_sb)
+                            .foregroundStyle(.pawkeyWhite1)
+                            .frame(maxWidth: .infinity, minHeight: 56)
+                    }
+                    .background(.green500)
+            }.onAppear {
+                mainTabViewModel.isHidden = true
+            }
+        }
+    }
+}
