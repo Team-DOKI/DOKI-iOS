1c9918ad96913a16423c04cb1af19fe572a85cc3
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-09-12 01:05:46 +0900

    [Add] #168 SearchField 컴포넌트 추가

--- PAWKEY/PAWKEY/Application/RootView.swift
+++ PAWKEY/PAWKEY/Application/RootView.swift
@@ -11,9 +11,11 @@ struct RootView: View {
 //    @EnvironmentObject var mainTabViewModel: MainTabViewModel
     @State var text1 = ""
     @State var text2 = ""
+    @State var text3 = ""
     
     var body: some View {
         MainTextField(placeholder: "text", text: $text1)
         MainTextField(placeholder: "text", text: $text2)
+        SearchField(placeholder: "text", text: $text3)
     }
 }
