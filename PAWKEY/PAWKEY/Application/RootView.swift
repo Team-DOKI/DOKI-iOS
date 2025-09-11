f4dd1ca6c0737e66bc61aa0c5d0578ac03f63a42
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-09-12 00:57:37 +0900

    [Feat] #168 TextField 컴포넌트 추가

--- PAWKEY/PAWKEY/Application/RootView.swift
+++ PAWKEY/PAWKEY/Application/RootView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct RootView: View {
 //    @EnvironmentObject var mainTabViewModel: MainTabViewModel
+    @State var text1 = ""
+    @State var text2 = ""
     
     var body: some View {
-        Text("RootView")
+        MainTextField(placeholder: "text", text: $text1)
+        MainTextField(placeholder: "text", text: $text2)
     }
 }
