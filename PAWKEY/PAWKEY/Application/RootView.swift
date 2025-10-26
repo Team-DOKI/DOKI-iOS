a70f9d1015e969dedea7120bbeb96609f271c737
권석기 <kwons3327@gmail.com>
2025-10-26 16:37:42 +0900

    [Feat] #182 AuthManager 추가

--- PAWKEY/PAWKEY/Application/RootView.swift
+++ PAWKEY/PAWKEY/Application/RootView.swift
@@ -8,28 +8,20 @@
 import SwiftUI
 
 struct RootView: View {
-    //    @EnvironmentObject var mainTabViewModel: MainTabViewModel
-    @State var breeds = [
-        Breed(name: "닥스훈트"),
-        Breed(name: "달마시안"),
-        Breed(name: "말라뮤트"),
-        Breed(name: "말티즈"),
-        Breed(name: "믹스견"),
-    ]
+    @EnvironmentObject var authManager: AuthManager
+    
     var body: some View {
-        VStack {
-            HStack {
-                CheckBox(text: "남아", isChecked: true)
-                CheckBox(text: "여아", isChecked: false)
-            }
-            
-            ScrollView {
-                ForEach(breeds.indices, id: \.self) { index in
-                    BreedItem(text: breeds[index].name, isChecked: breeds[index].isChecked) {
-                        breeds.indices.forEach { breeds[$0].isChecked = false }
-                        breeds[index].isChecked = true
+        Group {
+            switch authManager.authStatus {
+            case .loggedIn:
+                Text("로그인")
+            case .loggedOut:
+                Text("로그아웃")
+            case .loading:
+                Text("스플래쉬")
+                    .onAppear {
+                        authManager.checkLogin()
                     }
-                }
             }
         }
         .padding()
---
+++ PAWKEY/PAWKEY/Global/Manager/AuthManager.swift
@@ -0,0 +1,22 @@
+//
+//  AuthManager.swift
+//  PAWKEY
+//
+//  Created by a on 10/26/25.
+//
+
+import SwiftUI
+
+enum AuthState: String, CaseIterable {
+    case loggedIn
+    case loggedOut
+    case loading
+}
+
+class AuthManager: ObservableObject {
+    @Published var authStatus: AuthState = .loading
+    
+    func checkLogin() {
+        authStatus = .loggedIn
+    }
+}
