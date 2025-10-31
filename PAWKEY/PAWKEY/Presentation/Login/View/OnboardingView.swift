50aacd047ac17d516fa250bf0d9ab12192d0408d
권석기 <kwons3327@gmail.com>
2025-10-31 19:48:41 +0900

    [Add] #188 OnboardingView 추가

---
+++ PAWKEY/PAWKEY/Presentation/Login/View/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  PAWKEY
+//
+//  Created by a on 10/31/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
