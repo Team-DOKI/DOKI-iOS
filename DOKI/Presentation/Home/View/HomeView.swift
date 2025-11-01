357f41e4eb06dd75681af91f605bacd0c68c2ebe
권석기 <kwons3327@gmail.com>
2025-11-02 00:05:12 +0900

    [Feat] #190 RangeSlider 추가

--- DOKI/Presentation/Home/View/HomeView.swift
+++ DOKI/Presentation/Home/View/HomeView.swift
@@ -10,15 +10,16 @@ import SwiftUI
 struct HomeView: View {
     @StateObject var viewModel: HomeViewModel
     @State var item = "text1"
+    @State var value:CGFloat = 0
     
     var body: some View {
-        VStack {            
+        VStack {
+            Text("item: \(item)")
             SegmentedButton(items: ["text1", "text2", "text3"], selectedItem: $item)
-            Button {
-                viewModel.navigateToWalkRecord()
-            } label: {
-                Text("산책시작")
-            }
+            Text("value: \(value)")
+            RangeSlider(start: 10, end: 60, value: $value)
+            Text("hihi")
         }
+        .padding()
     }
 }
