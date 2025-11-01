298b7e1109f421df4d1a647959192a8a9ac4ada5
권석기 <kwons3327@gmail.com>
2025-11-02 00:19:05 +0900

    [Add] #190 아이콘 추가

---
+++ DOKI/Presentation/Walk/Component/WalkCourseCell.swift
@@ -0,0 +1,18 @@
+//
+//  WalkCourseCell.swift
+//  DOKI
+//
+//  Created by a on 11/2/25.
+//
+
+import SwiftUI
+
+struct WalkCourseCell: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    WalkCourseCell()
+}
