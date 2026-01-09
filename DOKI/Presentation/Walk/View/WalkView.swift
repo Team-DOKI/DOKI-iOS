f3c7817602d990550d0086b43ac0b04e559088eb
sem-git <lsmsm0327@naver.com>
2026-01-09 20:07:57 +0900

    [Feat] #200 산책 준비물 추가하기 구현

--- DOKI/Presentation/Walk/View/WalkView.swift
+++ DOKI/Presentation/Walk/View/WalkView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 
 struct WalkItem: Identifiable {
     let id = UUID()
-    let name: String
+    var name: String
     var isChecked: Bool
+    var isEditing: Bool
 }
 
 struct WalkView: View {
     @StateObject var viewModel: WalkViewModel
     
     @State private var items: [WalkItem] = [
-        WalkItem(name: "배변 봉투", isChecked: false),
-        WalkItem(name: "리드줄", isChecked: true),
-        WalkItem(name: "물", isChecked: true),
-        WalkItem(name: "간식", isChecked: false)
+        WalkItem(name: "배변 봉투", isChecked: false, isEditing: false),
+        WalkItem(name: "리드줄", isChecked: true, isEditing: false),
+        WalkItem(name: "물", isChecked: true, isEditing: false),
+        WalkItem(name: "간식", isChecked: false, isEditing: false)
     ]
+    
+    var isScrollable: Bool {
+        items.count > 7
+    }
     
     var body: some View {
         VStack(spacing: 0) {
+            
+            // MARK: - 오늘의 산책 TIP
+            
             HStack(spacing: 0) {
                 VStack(alignment: .leading, spacing: 2) {
                     Text("오늘의 산책 TIP")
@@ -43,50 +51,42 @@ struct WalkView: View {
                 .padding(.leading, 22)
                 
                 Spacer()
-                
-                //                Image(.imgWalkdog)
             }
             .frame(maxWidth: .infinity)
             .background(.defaultBackground)
             .cornerRadius(16)
             .overlay(
                 RoundedRectangle(cornerRadius: 16)
-                    .inset(by: 0.5)
                     .stroke(.defaultPrimary, lineWidth: 1)
             )
             .padding(.top, 16)
             .padding(.horizontal, 15)
+            
+            // MARK: - 산책 필수템
             
             VStack(alignment: .leading, spacing: 16) {
                 Text("산책 필수템")
                     .font(.subtitle)
                     .foregroundColor(.defaultDark)
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    ForEach($items) { $item in
-                        HStack(spacing: 8) {
-                            Button {
-                                item.isChecked.toggle()
-                            } label: {
-                                Image(item.isChecked ? .btnCheck : .btnUncheck)
-                            }
-                            
-                            Text(item.name)
-                                .font(.subDefault)
-                                .foregroundColor(item.isChecked ? .defaultDark : .defaultMiddle)
-                            
-                            Spacer()
-                            
-                            Image(.btnX)
-                        }
-                        .padding(.vertical, 8)
-                        
-                        Divider()
-                            .background(.defaultBright)
+                if isScrollable {
+                    ScrollView {
+                        itemList
                     }
+                    //                    .frame(maxHeight: 240)
+                    .scrollIndicators(.hidden)
+                } else {
+                    itemList
                 }
                 
                 Button {
+                    items.append(
+                        WalkItem(
+                            name: "",
+                            isChecked: false,
+                            isEditing: true
+                        )
+                    )
                 } label: {
                     Text("+ 추가하기")
                         .font(.subActive)
@@ -104,6 +104,8 @@ struct WalkView: View {
             
             Spacer()
             
+            // MARK: - 산책 기록 시작하기 버튼
+            
             MainButton(text: "산책 기록 시작하기", buttonState: .active2) {
                 viewModel.navigateToWalkRecord()
             }
@@ -115,5 +117,47 @@ struct WalkView: View {
             Text("산책")
                 .subtitle()
         })
+    }
+    
+    private var itemList: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach($items) { $item in
+                HStack(spacing: 8) {
+                    Button {
+                        item.isChecked.toggle()
+                    } label: {
+                        Image(item.isChecked ? .btnCheck : .btnUncheck)
+                    }
+                    
+                    if item.isEditing {
+                        TextField("준비물을 작성해주세요.", text: $item.name)
+                            .font(.subDefault)
+                            .foregroundColor(.defaultMiddle)
+                            .submitLabel(.done)
+                            .onSubmit {
+                                item.isEditing = false
+                            }
+                    } else {
+                        Text(item.name)
+                            .font(.subDefault)
+                            .foregroundColor(
+                                item.isChecked ? .defaultDark : .defaultMiddle
+                            )
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        items.removeAll { $0.id == item.id }
+                    } label: {
+                        Image(.btnX)
+                    }
+                }
+                .padding(.vertical, 8)
+                
+                Divider()
+                    .background(.defaultBright)
+            }
+        }
     }
 }
