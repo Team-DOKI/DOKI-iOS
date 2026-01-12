36069071f2677f15b39d9f92dfc082d7b4cd97b4
seokki2 <rnjstjrrl12@gmail.com>
2026-01-12 21:19:20 +0900

    [Fix] #205 메뉴관련 컴포넌트 분리

--- DOKI/Presentation/MyPage/Component/PetProfile.swift
+++ DOKI/Presentation/MyPage/Component/PetProfile.swift
@@ -11,37 +11,40 @@ struct PetProfile: View {
     let name: String
     let dbti: String
     let petInfo: String
+    let action: ()->()
     
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("반려견 프로필")
-                    .subActive(color: .defaultBackground)
-                Spacer()
-            }
-            .frame(height: 36)
-            .padding(.horizontal, 16)
-            .padding(.vertical, 10)
-            .background(.defaultPrimary)
-            HStack(spacing: 16) {
-                Image(.imgDefaultprofile)
-                    .frame(width: 60, height: 60)
-                VStack(alignment: .leading, spacing: 4) {
-                    AddressTag(text: dbti)
-                    HStack(spacing: 4) {
-                        Text(name)
-                            .subtitle()
-                        Text(petInfo)
-                            .subDefault(color: .defaultMiddle)
-                    }
+        Button(action: action) {
+            VStack(spacing: 0) {
+                HStack {
+                    Text("반려견 프로필")
+                        .subActive(color: .defaultBackground)
+                    Spacer()
                 }
-                Spacer()
+                .frame(height: 36)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+                .background(.defaultPrimary)
+                HStack(spacing: 16) {
+                    Image(.imgDefaultprofile)
+                        .frame(width: 60, height: 60)
+                    VStack(alignment: .leading, spacing: 4) {
+                        AddressTag(text: dbti)
+                        HStack(spacing: 4) {
+                            Text(name)
+                                .subtitle()
+                            Text(petInfo)
+                                .subDefault(color: .defaultMiddle)
+                        }
+                    }
+                    Spacer()
+                }
+                .frame(height: 100)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 16)
+                .background(.defaultBackground)
             }
-            .frame(height: 100)
-            .padding(.vertical, 10)
-            .padding(.horizontal, 16)
-            .background(.defaultBackground)
+            .cornerRadius(8)
         }
-        .cornerRadius(8)
     }
 }
--- DOKI/Presentation/MyPage/View/MyPageView.swift
+++ DOKI/Presentation/MyPage/View/MyPageView.swift
@@ -20,15 +20,30 @@ struct MyPageView: View {
                         email: "hello@gmail.com",
                         action: {}
                     )
-                                        
+                    
                     PetProfile(
                         name: "단지",
                         dbti: "동네인기스타bbb",
-                        petInfo: "6개월 / 여아 / 견종 이름"
+                        petInfo: "6개월 / 여아 / 견종 이름",
+                        action: {}
                     )
-
-                    WalkRouteManage()
-                    Setting()
+                    
+                    MenuSection(title: "산책 루트 관리") {
+                        Group {
+                            MenuItem(title: "내가 기록한 산책", icon:  Image(.icRecord), action: {})
+                            MenuItem(title: "저장목록", icon:  Image(.icSave), action: {})
+                            MenuItem(title: "내가 남긴 후기", icon:  Image(.icReview), action: {})
+                        }
+                    }
+                    
+                    MenuSection(title: "설정") {
+                        Group {
+                            MenuItem(title: "활동 범위 설정", icon:  Image(.icLocation), action: {})
+                            MenuItem(title: "앱 정보", icon:  Image(.icInfo), action: {})
+                            MenuItem(title: "로그아웃", icon:  Image(.icLogout), action: {})
+                            MenuItem(title: "탈퇴하기", icon:  Image(.icQuit), action: {})
+                        }
+                    }
                 }
                 .padding(.horizontal, 16)
                 .padding(.vertical, 22)
